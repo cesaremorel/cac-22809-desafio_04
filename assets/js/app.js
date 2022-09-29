9fa2480c3a7bf3b23abd4a50b61099bad2faecdb
Cesar Morel <cesaremorel@gmail.com>
2022-09-29 14:27:23 -0300

    Evita numeros repetidos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,23 @@
-
-const getQuiniNumbersArray = () => {
-    // Numeros desde el 1 al 45 inclusive
-    let numbers = []
-    for ( var i = 0; i < 6; i++ ){
-        numbers[i] = Math.floor(Math.random() * 45)
+const getNumberArrayFromTo = (from, to) => {
+    let numbers = [];
+    for ( i = from; i <= to; i++){
+        numbers.push(i)
     }
     return numbers
+}
+
+const getQuiniNumbersArray = () => { 
+    // Numeros desde el 1 al 45 inclusive
+    let remainingNumbers = getNumberArrayFromTo(1,45)
+    let limit = 6
+    let randomNumbers = []
+
+    for ( i = 0; i < limit; i++){
+        var pickId = Math.floor(Math.random() * remainingNumbers.length )
+        randomNumbers.push(remainingNumbers[pickId])
+        remainingNumbers.splice(pickId, 1);
+    }
+    return randomNumbers.sort((a,b) => a-b)
 }
 
 const renderQuiniCard = () => {
@@ -13,7 +25,7 @@ const renderQuiniCard = () => {
     quini6Div.innerHTML = ''
     var numbers = getQuiniNumbersArray()
     numbers.forEach(number => {
-        quini6Div.innerHTML += `<span class="badge bg-info text-dark m-2">${number}</span>`
+        quini6Div.innerHTML += `<span class="badge bg-info text-dark m-2">${String(number).padStart(2, '0')}</span>`
     });
 }
 
